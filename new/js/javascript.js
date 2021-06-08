f03d87285dead571a8300f5a00cee21709f28502
JamesCheek21 <jamesfrancis80@aol.com>
2021-06-08 02:59:53 +0100

    Added about section and modelling pages

--- new/js/javascript.js
+++ new/js/javascript.js
@@ -12,4 +12,28 @@ $(document).ready(function() {
       $(this).toggleClass("active");
       $(".navbar-menu").toggleClass("active");
     });
+
+    $(".works").magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      gallery:{
+        enabled: true
+      }
+    });
+});
+$(function() {
+	$('nav.menu a').bind('click',function(event){
+		var $anchor = $(this);
+
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1000);
+		/*
+		if you don't want to use the easing effects:
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1000);
+		*/
+		event.preventDefault();
+	});
 });
