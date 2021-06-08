1ce1423f97e91321e8eb4eea74464483fc110e54
JamesCheek21 <jamesfrancis80@aol.com>
2021-06-08 23:40:18 +0100

    Fixed contact form

--- js/javascript.js
+++ js/javascript.js
@@ -20,6 +20,49 @@ $(document).ready(function() {
         enabled: true
       }
     });
+    $('.submit').click(function (event) {
+
+    console.log('Clicked button');
+
+    var name = $('#contact-name').val();
+    var email = $('#contact-email').val();
+    var subject = $('#contact-subject').val();
+    var message = $('#contact-message').val();
+    var statusElement = $('.status');
+    statusElement.empty();
+
+    if(name.length >= 1){
+      statusElement.append('<div>Name is valid</div>');
+    }
+    else{
+      event.preventDefault();
+      statusElement.append('<div>Name is invalid</div>');
+    }
+
+    if (email.length > 5 && email.includes('@') && email.includes('.')){
+      statusElement.append('<div>Email is valid</div>');
+    }
+    else{
+      event.preventDefault();
+      statusElement.append('<div>Email is invalid</div>');
+    }
+
+    if(subject.length >= 2){
+      statusElement.append('<div>Subject is valid</div>');
+    }
+    else{
+      event.preventDefault();
+      statusElement.append('<div>Subject is invalid</div>');
+    }
+
+    if(message.length >= 30){
+      statusElement.append('<div>Message is valid</div>');
+    }
+    else{
+      event.preventDefault();
+      statusElement.append('<div>Message is invalid</div>');
+    }
+  });
 });
 $(function() {
 	$('nav.menu a').bind('click',function(event){
